b4077c79779c6dd8b20b8039a314bc750bd27993
Kevin Yuan <kevin-yuan@live.com>
2026-05-15 18:49:24 +0100

    Release 1.0.5: replace builtin-modules with node builtinModules

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
 import esbuild from "esbuild";
 import process from "process";
-import builtins from "builtin-modules";
+import { builtinModules } from "module";
 import fs from "fs";
 import { createRequire } from "module";
 
@@ -109,7 +109,8 @@ const context = await esbuild.context({
     "@lezer/common",
     "@lezer/highlight",
     "@lezer/lr",
-    ...builtins,
+    ...builtinModules,
+    ...builtinModules.map(m => `node:${m}`),
   ],
   plugins: [fixTikzjaxPaths, fixPunycode],
   platform: "node",
